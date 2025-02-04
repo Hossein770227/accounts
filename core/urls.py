1922523939d7bce341f938104ac99414039caf6a
hossein <hossein.h.a770227@gmail.com>
2025-02-04 12:29:29 +0330

    add user_register_view

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('signup/', views.UserRegisterView.as_view(), name='user_regster')
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import random
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.utils.translation import gettext as _
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
-# Create your views here.
+from datetime import datetime, timedelta, timezone
+import pytz
+
+
+from .forms import UserRegisterForm, VerifyCodeForm
+from utils import send_otp_code
+from .models import OtpCode, MyUser
+
+class UserRegisterView(View):
+    form_class = UserRegisterForm
+    def get(self, request):
+        form= self.form_class
+        return render(request, 'user_register.html', {'form':form})
+    
+    def post(self, request):
+        form =self.form_class(request.POST)
+        if form.is_valid():
+            otp = OtpCode.objects.filter(phone_number = form.cleaned_data['phone'])
+            if otp.exists():
+                messages.error(request, _('we send your code already'))
+                return redirect('core:verify_code')
+            random_code = random.randint(1000, 9999)
+            send_otp_code(form.cleaned_data['phone'], random_code)
+            OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
+            request.session['user_registration_info']={
+                'first_name':form.cleaned_data['first_name'],
+                'last_name':form.cleaned_data['last_name'],
+                'phone_number':form.cleaned_data['phone'],
+                'password':form.cleaned_data['password2'],
+            }
+            messages.success(request, _('we sent you a code'))
+            return redirect('core:verify_code')
+        return render(request, 'user_register.html', {'form':form})
