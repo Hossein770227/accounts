d4d1b610dd3b072aa4be411b6497f5c7ff7096b3
hossein <hossein.h.a770227@gmail.com>
2025-02-04 12:00:11 +0330

    add forms.py for core app

--- core/models.py
+++ core/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser
 
+from .managers import MyUserManager
+
 class MyUser(AbstractBaseUser):
     first_name= models.CharField(verbose_name=_('first name'), max_length=100)
     last_name= models.CharField(verbose_name=_('last name'), max_length=100)
@@ -15,13 +17,13 @@ class MyUser(AbstractBaseUser):
         verbose_name_plural = _('my users')
 
 
-    # objects = MyUserManager()
+    objects = MyUserManager()
 
     USERNAME_FIELD ='phone_number'
     REQUIRED_FIELDS =['first_name', 'last_name']
     
     def __str__(self):
-        return {self.first_name}
+        return f'{self.first_name}:{self.last_name}' 
 
     def has_perm(self, perm, obj=None):
         return True
